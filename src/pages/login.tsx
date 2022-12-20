dc65e3147ba8ee5810c70c14d3442358107e4f08
marcos <marcosrbal@outlook.com>
2022-12-20 15:11:34 +0100

    add redirect to Home page after successful login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,11 +1,15 @@
 import { auth, provider } from '../config/firebase';
 import { signInWithPopup } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
 
 
 const Login = () => {
+    const navigate = useNavigate();
+
     const signInWithGoogle = async () => {
         const result = await signInWithPopup(auth, provider);
         console.log(result);
+        navigate("/");
     };
 
     return ( 
