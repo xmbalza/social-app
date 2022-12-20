fe324fa019284db5f9ef47e3aca6be20ab1192db
marcos <marcosrbal@outlook.com>
2022-12-20 15:20:42 +0100

    add user and user image display

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,11 +1,19 @@
 import { Link } from 'react-router-dom'
+import { auth } from '../config/firebase';
 
 const Navbar = () => {
     return ( 
         <div>
             <Link to="/">Home</Link>
             <Link to="/login">Login</Link>
+
+        <div>
+            <p>{auth.currentUser?.displayName}</p>
+            <img src={auth.currentUser?.photoURL || ""} alt="user image" width="100" height="100" referrerPolicy="no-referrer" />
         </div>
+
+        </div>
+        
      );
 }
  
