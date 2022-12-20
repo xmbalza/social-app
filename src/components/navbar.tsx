2e416919aa071f622505e6c871d82990f357c1cb
marcos <marcosrbal@outlook.com>
2022-12-20 15:37:49 +0100

    add basic css styling

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,20 +1,23 @@
 import { Link } from 'react-router-dom'
 import { auth } from '../config/firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
 
 const Navbar = () => {
-    return ( 
-        <div>
-            <Link to="/">Home</Link>
-            <Link to="/login">Login</Link>
+    //firebase hooks lib functionality manages state of current logged in user. If user changes, this function keep track and updates accordingly. Pass "auth" from config to reference user.
+    const [user] = useAuthState(auth);
 
-        <div>
-            <p>{auth.currentUser?.displayName}</p>
-            <img src={auth.currentUser?.photoURL || ""} alt="user image" width="100" height="100" referrerPolicy="no-referrer" />
+    return (
+        <div className="navbar">
+            <div className='links'>
+                <Link to="/">Home</Link>
+                <Link to="/login">Login</Link>
+            </div>
+            <div className='user'>
+                <p>{user?.displayName}</p>
+                <img src={user?.photoURL || ""} alt="user image" width="20" height="20" referrerPolicy="no-referrer" />
+            </div>
         </div>
-
-        </div>
-        
-     );
+    );
 }
  
 export default Navbar;
