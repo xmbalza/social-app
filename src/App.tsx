0efa73046a1d7ff13d2432beab98b4280e1bebb4
marcos <marcosrbal@outlook.com>
2022-12-20 13:53:00 +0100

    create pages/routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Main from './pages/main';
+import Login from './pages/login';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
