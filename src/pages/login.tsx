1129148b0d4dcef92ca8a5eb3cfb496db478218d
marcos <marcosrbal@outlook.com>
2022-12-20 15:06:06 +0100

    integrate google auth with login component. Add firebase config file to .gitignore for security

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,6 +1,18 @@
+import { auth, provider } from '../config/firebase';
+import { signInWithPopup } from 'firebase/auth';
+
+
 const Login = () => {
+    const signInWithGoogle = async () => {
+        const result = await signInWithPopup(auth, provider);
+        console.log(result);
+    };
+
     return ( 
-        <div> Login </div>
+        <div>
+            <p>Sign in with Google to continue </p>
+            <button onClick={signInWithGoogle}>Sign in with Google</button>
+        </div>
      );
 }
  
