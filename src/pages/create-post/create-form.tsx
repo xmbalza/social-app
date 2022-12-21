cc2c4c56f23fcf9737ff9c4e76a9e3cd50f1d9b3
marcos <marcosrbal@outlook.com>
2022-12-21 18:13:03 +0100

    add CSS to createForm component. add redirect to home page after post submit.

--- src/pages/create-post/create-form.tsx
+++ src/pages/create-post/create-form.tsx
@@ -4,6 +4,7 @@ import { yupResolver } from '@hookform/resolvers/yup';
 import { addDoc, collection} from 'firebase/firestore';
 import { auth, db } from '../../config/firebase';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom'
 
 interface CreateFormData {
     title: string;
@@ -13,7 +14,7 @@ interface CreateFormData {
 //defines schema for yup validation
 const CreateForm = () => {
     const [user] = useAuthState(auth);
-
+    const navigate = useNavigate();
 
     const schema = yup.object().shape({
         title: yup.string().required('You must add a title.'),
@@ -32,7 +33,9 @@ const CreateForm = () => {
             ...data,
             username: user?.displayName,
             userId: user?.uid,
-        })
+        });
+
+        navigate('/');
     }
 
     return ( 
@@ -41,7 +44,7 @@ const CreateForm = () => {
             <p style={{color: 'red'}}> {errors.title?.message}</p>
             <textarea placeholder='description...' {...register('description')}/>
             <p style={{color: 'red'}}> {errors.description?.message}</p>
-            <input type='submit' />
+            <input type='submit' className='submitForm' />
         </form>
     );
 }
